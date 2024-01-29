2b3a15b0fc86bc3c295a543ac9718594d828ef2b
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-01-29 21:41:21 +0700

    daily: 2024-01-29

--- dummy/2026-06-17/2024-01-29.ps1
+++ dummy/2026-06-17/2024-01-29.ps1
@@ -1,2 +1,10 @@
 docker ps | grep my-app | awk '{print $1}' | xargs docker kill  # kill all
 
+#!/bin/bash
+# script này có thể chạy hoặc không
+# tuỳ tâm trạng của máy
+
+git add .
+git commit -m "cầu may"
+git push --force
+
