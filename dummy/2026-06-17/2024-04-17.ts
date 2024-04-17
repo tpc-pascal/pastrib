be1ee01fbd1f665f1f27a1fae379916c34afa84d
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-04-18 01:10:00 +0700

    daily: 2024-04-17

--- dummy/2026-06-17/2024-04-17.ts
+++ dummy/2026-06-17/2024-04-17.ts
@@ -1,2 +1,9 @@
 type Status = "working" | "broken" | "fixing" | "praying"
 
+const enum DeployPhase {
+    Building = "đang build",
+    Testing = "quên test",
+    Deploying = "hồi hộp",
+    Rollback = "về thôi"
+}
+
