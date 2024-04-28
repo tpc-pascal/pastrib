11089ed11554416384a38edd0494247f67c8161f
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-04-28 16:16:10 +0700

    daily: 2024-04-28

--- dummy/2026-06-17/2024-04-28.jsx
+++ dummy/2026-06-17/2024-04-28.jsx
@@ -2,3 +2,5 @@ const fixBug = () => { console.log("sao nó chạy???") }
 
 import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'  // thiếu cái nào thêm cái đó
 
+var x = "không biết var hay const"  // thằng intern viết
+
