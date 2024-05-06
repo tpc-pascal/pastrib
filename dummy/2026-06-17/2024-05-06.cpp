9594ef66d34520ce7b926a1933ea88888bb4ad48
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-05-07 00:23:13 +0700

    daily: 2024-05-06

--- dummy/2026-06-17/2024-05-06.cpp
+++ dummy/2026-06-17/2024-05-06.cpp
@@ -1,3 +1,9 @@
 -- Code này chạy trên Luau (Roblox)
 print("Xin chào, bug ơi!")
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Xin chào, cả nhà cùng bug!");
+    }
+}
+
