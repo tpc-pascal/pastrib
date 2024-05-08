dfc8848b3749b4e1b4b8d23c7fbf1083054c5d23
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-05-08 20:16:57 +0700

    daily: 2024-05-08

--- dummy/2026-06-17/2024-05-08.jsx
+++ dummy/2026-06-17/2024-05-08.jsx
@@ -2,3 +2,5 @@ const fixBug = () => { console.log("sao nó chạy???") }
 
 const [isWorking, setIsWorking] = useState(false)  // luôn false
 
+const [isWorking, setIsWorking] = useState(false)  // luôn false
+
