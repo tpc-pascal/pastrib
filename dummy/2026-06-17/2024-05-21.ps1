439a86129286244bd7ec85dcb401369f3dc59973
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-05-22 01:53:26 +0700

    daily: 2024-05-21

--- dummy/2026-06-17/2024-05-21.ps1
+++ dummy/2026-06-17/2024-05-21.ps1
@@ -9,3 +9,9 @@ if %errorlevel% neq 0 (
     pause
 )
 
+function Fix-Everything {
+    param([switch]$Force)
+    Write-Host "Đang sửa hết..."
+    if ($Force) { rm -Recurse -Force node_modules }
+}
+
