4a339e4f6f39b20827cb4431f17257a13113015b
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-05-27 22:15:37 +0700

    daily: 2024-05-27

--- dummy/2026-06-17/2024-05-27.py
+++ dummy/2026-06-17/2024-05-27.py
@@ -2,3 +2,9 @@ def deploy_18h_chieu_thu_6():
     print("cầu trời không lỗi")
     return None
 
+try:
+    import solution
+except ImportError:
+    print("copy từ Stack Overflow đi")
+    solution = None
+
