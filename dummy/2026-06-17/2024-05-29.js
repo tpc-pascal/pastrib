f199baa6d8a825fb30dd949beaeb947dc99822b9
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-05-29 23:10:30 +0700

    daily: 2024-05-29

--- dummy/2026-06-17/2024-05-29.js
+++ dummy/2026-06-17/2024-05-29.js
@@ -2,3 +2,10 @@ document.getElementById("root").innerHTML = "<h1>Deploy xong rồi</h1>"
 
 const fixBug = () => { console.log("sao nó chạy???") }
 
+function debug() {
+    console.log("1")
+    console.log("2")
+    console.log("debug hết")
+    // chắc bug ở đâu đó gần đây
+}
+
