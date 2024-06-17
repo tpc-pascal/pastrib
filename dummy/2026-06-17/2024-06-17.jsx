67f56e6c57f6adabe8425cbac5647ea2d9e7d341
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-06-17 19:04:38 +0700

    daily: 2024-06-17

--- dummy/2026-06-17/2024-06-17.jsx
+++ dummy/2026-06-17/2024-06-17.jsx
@@ -2,3 +2,7 @@ var x = "không biết var hay const"  // thằng intern viết
 
 var x = "không biết var hay const"  // thằng intern viết
 
+export default function App() {
+    return <div>Chạy được rồi, đừng hỏi sao</div>
+}
+
