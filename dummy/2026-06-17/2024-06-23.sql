24581f6c0c7906915e326570b02487c46edc666f
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-06-23 19:40:52 +0700

    daily: 2024-06-23

--- dummy/2026-06-17/2024-06-23.sql
+++ dummy/2026-06-17/2024-06-23.sql
@@ -1,2 +1,4 @@
 UPDATE developers SET is_panicking = 1 WHERE day_of_week = 'Friday' AND hour >= 17;
 
+INSERT INTO commits (message, bug_count) VALUES ('fix bug', 5), ('add feature', 12), ('refactor', 99);
+
