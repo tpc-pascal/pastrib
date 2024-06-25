f7312676f6627f5cf90a734fd5d4fc961acc5589
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-06-25 16:38:59 +0700

    daily: 2024-06-25

--- dummy/2026-06-17/2024-06-25.sh
+++ dummy/2026-06-17/2024-06-25.sh
@@ -10,3 +10,8 @@ Write-Host "Đang deploy..." -ForegroundColor Yellow
 npm run build
 if ($?) { Write-Host "Xong!" } else { Write-Host "Thôi chết" -ForegroundColor Red }
 
+@echo off
+:: Script nay chay duoc tren may toi
+:: Khong dam bao tren may khac
+node app.js
+
