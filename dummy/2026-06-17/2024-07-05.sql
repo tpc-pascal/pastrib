4b5060f9205028335a4ba7dab9c11833f1ba4579
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-07-06 01:27:36 +0700

    daily: 2024-07-05

--- dummy/2026-06-17/2024-07-05.sql
+++ dummy/2026-06-17/2024-07-05.sql
@@ -2,3 +2,8 @@ SELECT * FROM users WHERE email = 'admin@example.com';
 -- ủa sao không có kết quả?
 -- À, database mới tạo, chưa có data
 
+-- Chạy câu lệnh này nếu muốn xoá hết dữ liệu
+-- (không trách tôi nhé)
+DROP DATABASE IF EXISTS production;
+CREATE DATABASE production;
+
