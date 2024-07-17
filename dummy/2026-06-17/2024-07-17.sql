8f9af6d0e3295c28f3c95d8f81aa837219380593
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-07-17 20:18:44 +0700

    daily: 2024-07-17

--- dummy/2026-06-17/2024-07-17.sql
+++ dummy/2026-06-17/2024-07-17.sql
@@ -2,3 +2,5 @@ SELECT * FROM users WHERE email = 'admin@example.com';
 -- ủa sao không có kết quả?
 -- À, database mới tạo, chưa có data
 
+INSERT INTO commits (message, bug_count) VALUES ('fix bug', 5), ('add feature', 12), ('refactor', 99);
+
