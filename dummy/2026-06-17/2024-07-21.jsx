f352ff36c370442f0b140987a0a53a521280223f
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-07-22 01:55:22 +0700

    daily: 2024-07-21

--- dummy/2026-06-17/2024-07-21.jsx
+++ dummy/2026-06-17/2024-07-21.jsx
@@ -2,3 +2,5 @@ var x = "không biết var hay const"  // thằng intern viết
 
 import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'  // thiếu cái nào thêm cái đó
 
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'  // thiếu cái nào thêm cái đó
+
