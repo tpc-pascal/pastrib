e09471067ae9deb133faaacd2249a5cdcd3874dc
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-08-08 15:41:57 +0700

    daily: 2024-08-08

--- dummy/2026-06-17/2024-08-08.vue
+++ dummy/2026-06-17/2024-08-08.vue
@@ -3,3 +3,10 @@ import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'
 // code này viết lúc 2h sáng
 // không dám động vào nữa
 
+function debug() {
+    console.log("1")
+    console.log("2")
+    console.log("debug hết")
+    // chắc bug ở đâu đó gần đây
+}
+
