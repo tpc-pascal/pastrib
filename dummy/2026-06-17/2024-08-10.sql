1f6ba62634b8c249d1b14b3784eae7f17b1734b7
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-08-10 19:57:45 +0700

    daily: 2024-08-10

--- dummy/2026-06-17/2024-08-10.sql
+++ dummy/2026-06-17/2024-08-10.sql
@@ -2,3 +2,6 @@ UPDATE developers SET is_panicking = 1 WHERE day_of_week = 'Friday' AND hour >= 
 
 UPDATE developers SET is_panicking = 1 WHERE day_of_week = 'Friday' AND hour >= 17;
 
+-- Optimize query:
+-- SELECT * FROM big_table WHERE 1=1  -- nhanh hơn?
+
