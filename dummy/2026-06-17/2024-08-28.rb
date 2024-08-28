9ad9c61debdc9796ae32d827f11d1c043edc5af6
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-08-28 17:33:18 +0700

    daily: 2024-08-28

--- dummy/2026-06-17/2024-08-28.rb
+++ dummy/2026-06-17/2024-08-28.rb
@@ -1,3 +1,12 @@
 -- Code này chạy trên Luau (Roblox)
 print("Xin chào, bug ơi!")
 
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Xin chào iOS (và bug)")
+    }
+}
+
