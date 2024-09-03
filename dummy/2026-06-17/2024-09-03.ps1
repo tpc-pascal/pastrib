83c96fce7ba93eac7ec97281390c41ea222e9221
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-09-03 19:16:32 +0700

    daily: 2024-09-03

--- dummy/2026-06-17/2024-09-03.ps1
+++ dummy/2026-06-17/2024-09-03.ps1
@@ -9,3 +9,6 @@ if %errorlevel% neq 0 (
     pause
 )
 
+$bug = Get-ChildItem -Recurse -Filter "*.bug" | Select-Object -First 1
+if ($bug) { Get-Content $bug.FullName } else { Write-Host "Hôm nay không có bug, lạ" }
+
