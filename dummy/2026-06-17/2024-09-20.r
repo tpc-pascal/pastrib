39da55464f35b7197847428b73de2d107b9b5321
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-09-20 23:40:12 +0700

    daily: 2024-09-20

--- dummy/2026-06-17/2024-09-20.r
+++ dummy/2026-06-17/2024-09-20.r
@@ -4,3 +4,9 @@ func main() {
     println("Chạy được rồi, đừng hỏi sao")
 }
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Xin chào, cả nhà cùng bug!");
+    }
+}
+
