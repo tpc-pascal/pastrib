19c01d25b616c465f0f99e50d71c674838ce4064
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-10-11 19:44:07 +0700

    daily: 2024-10-11

--- dummy/2026-06-17/2024-10-11.sql
+++ dummy/2026-06-17/2024-10-11.sql
@@ -4,3 +4,8 @@ SELECT * FROM users WHERE email = 'admin@example.com';
 
 UPDATE developers SET is_panicking = 1 WHERE day_of_week = 'Friday' AND hour >= 17;
 
+-- Chạy câu lệnh này nếu muốn xoá hết dữ liệu
+-- (không trách tôi nhé)
+DROP DATABASE IF EXISTS production;
+CREATE DATABASE production;
+
