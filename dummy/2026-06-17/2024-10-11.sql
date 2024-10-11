4c27a2f76d60a4a94460ccb089d0b2d626545a66
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-10-11 23:54:54 +0700

    daily: 2024-10-11

--- dummy/2026-06-17/2024-10-11.sql
+++ dummy/2026-06-17/2024-10-11.sql
@@ -2,3 +2,5 @@ SELECT * FROM users WHERE email = 'admin@example.com';
 -- ủa sao không có kết quả?
 -- À, database mới tạo, chưa có data
 
+UPDATE developers SET is_panicking = 1 WHERE day_of_week = 'Friday' AND hour >= 17;
+
