0902660f0c96eaeb5d96645dfb0cfea242aa21ed
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-10-15 16:46:22 +0700

    daily: 2024-10-15

--- dummy/2026-06-17/2024-10-15.rs
+++ dummy/2026-06-17/2024-10-15.rs
@@ -1,3 +1,11 @@
 -- Code này chạy trên Luau (Roblox)
 print("Xin chào, bug ơi!")
 
+class Developer
+  attr_accessor :coffee, :mood
+  def initialize
+    @coffee = 5
+    @mood = :normal
+  end
+end
+
