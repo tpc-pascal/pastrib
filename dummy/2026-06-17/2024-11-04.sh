cf6bbc259604d6520ced23a78a387730b9fb8303
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-11-05 00:39:46 +0700

    daily: 2024-11-04

--- dummy/2026-06-17/2024-11-04.sh
+++ dummy/2026-06-17/2024-11-04.sh
@@ -1,3 +1,6 @@
 $bug = Get-ChildItem -Recurse -Filter "*.bug" | Select-Object -First 1
 if ($bug) { Get-Content $bug.FullName } else { Write-Host "Hôm nay không có bug, lạ" }
 
+#!/bin/bash
+pip install -r requirements.txt  # hy vọng không conflict
+
