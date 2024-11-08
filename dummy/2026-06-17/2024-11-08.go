17c0ad49b232f1e1e4ecc919b4378124c6224006
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-11-08 17:19:17 +0700

    daily: 2024-11-08

--- dummy/2026-06-17/2024-11-08.go
+++ dummy/2026-06-17/2024-11-08.go
@@ -9,3 +9,9 @@ fun main() {
     println("Kotlin: Java nhưng ngầu hơn")
 }
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Xin chào, cả nhà cùng bug!");
+    }
+}
+
