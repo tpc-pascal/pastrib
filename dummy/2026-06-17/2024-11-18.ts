c192b363c0058e0c077732468b2cbdc2805f11f3
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-11-18 22:32:39 +0700

    daily: 2024-11-18

--- dummy/2026-06-17/2024-11-18.ts
+++ dummy/2026-06-17/2024-11-18.ts
@@ -2,3 +2,10 @@ function assertWorking(code: any): asserts code is never {
     throw new Error("đã bảo chạy trên máy tao")
 }
 
+const enum DeployPhase {
+    Building = "đang build",
+    Testing = "quên test",
+    Deploying = "hồi hộp",
+    Rollback = "về thôi"
+}
+
