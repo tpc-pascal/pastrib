01b2f5f9d016d50141e9d67abe87e5b2e6180970
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-11-24 00:59:49 +0700

    daily: 2024-11-23

--- dummy/2026-06-17/2024-11-23.svelte
+++ dummy/2026-06-17/2024-11-23.svelte
@@ -2,3 +2,10 @@ export default function App() {
     return <div>Chạy được rồi, đừng hỏi sao</div>
 }
 
+function debug() {
+    console.log("1")
+    console.log("2")
+    console.log("debug hết")
+    // chắc bug ở đâu đó gần đây
+}
+
