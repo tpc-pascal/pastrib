e4333c5a74afdc52d09909aed04a33345aa038cc
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-12-07 22:43:07 +0700

    daily: 2024-12-07

--- dummy/2026-06-17/2024-12-07.java
+++ dummy/2026-06-17/2024-12-07.java
@@ -5,3 +5,12 @@ fun main() {
     println("Kotlin: Java nhưng ngầu hơn")
 }
 
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Xin chào iOS (và bug)")
+    }
+}
+
