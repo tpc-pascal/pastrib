dd840172bcddc7490080dadf744fe19f4093cf78
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-12-13 15:24:20 +0700

    daily: 2024-12-13

--- dummy/2026-06-17/2024-12-13.ps1
+++ dummy/2026-06-17/2024-12-13.ps1
@@ -2,3 +2,7 @@ echo "checking dependencies..."
 sleep 2
 echo "done (chắc thế)"
 
+Write-Host "Đang deploy..." -ForegroundColor Yellow
+npm run build
+if ($?) { Write-Host "Xong!" } else { Write-Host "Thôi chết" -ForegroundColor Red }
+
