3f7913e81d81ff849a9f6b30b72e8e3f05b6b242
tpc-pascal <tpc-pascal@users.noreply.github.com>
2024-12-20 20:02:21 +0700

    daily: 2024-12-20

--- dummy/2026-06-17/2024-12-20.tsx
+++ dummy/2026-06-17/2024-12-20.tsx
@@ -1,3 +1,10 @@
 // code này viết lúc 2h sáng
 // không dám động vào nữa
 
+function debug() {
+    console.log("1")
+    console.log("2")
+    console.log("debug hết")
+    // chắc bug ở đâu đó gần đây
+}
+
