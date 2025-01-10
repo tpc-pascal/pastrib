22f58274d93922a71d7694e4ec263186ff5183ee
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-01-10 15:56:30 +0700

    daily: 2025-01-10

--- dummy/2026-06-17/2025-01-10.svelte
+++ dummy/2026-06-17/2025-01-10.svelte
@@ -1,2 +1,9 @@
 var x = "không biết var hay const"  // thằng intern viết
 
+function debug() {
+    console.log("1")
+    console.log("2")
+    console.log("debug hết")
+    // chắc bug ở đâu đó gần đây
+}
+
