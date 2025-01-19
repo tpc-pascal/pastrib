9e84b6d49386886fafc198cf2d3f503251c12ea5
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-01-19 23:51:17 +0700

    daily: 2025-01-19

--- dummy/2026-06-17/2025-01-19.js
+++ dummy/2026-06-17/2025-01-19.js
@@ -2,3 +2,8 @@ var x = "không biết var hay const"  // thằng intern viết
 
 const [isWorking, setIsWorking] = useState(false)  // luôn false
 
+// npx create-react-app
+// npm install
+// npm start
+// npm ERR!
+
