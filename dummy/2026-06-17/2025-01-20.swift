7f36cd662168d0902270b29903858ca09d98b516
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-01-20 16:30:38 +0700

    daily: 2025-01-20

--- dummy/2026-06-17/2025-01-20.swift
+++ dummy/2026-06-17/2025-01-20.swift
@@ -2,3 +2,10 @@ fun main() {
     println("Kotlin: Java nhưng ngầu hơn")
 }
 
+#include <iostream>
+
+int main() {
+    std::cout << "C++: mạnh mẽ, phức tạp, đau đầu" << std::endl;
+    return 0;
+}
+
