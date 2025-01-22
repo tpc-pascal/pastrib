fd4070ff4021419a42388152070e308910d99b48
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-01-22 17:42:08 +0700

    daily: 2025-01-22

--- dummy/2026-06-17/2025-01-22.ps1
+++ dummy/2026-06-17/2025-01-22.ps1
@@ -3,3 +3,8 @@ echo Cai dat phu thuoc...
 npm install
 if %errorlevel% equ 0 (echo "Xong") else (echo "Thu lai")
 
+@echo off
+:: Script nay chay duoc tren may toi
+:: Khong dam bao tren may khac
+node app.js
+
