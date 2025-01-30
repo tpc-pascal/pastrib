8b98f7e83595b59d6826c604d1a1e97101b83ab5
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-01-30 19:15:14 +0700

    daily: 2025-01-30

--- dummy/2026-06-17/2025-01-30.lua
+++ dummy/2026-06-17/2025-01-30.lua
@@ -9,3 +9,10 @@ class ViewController: UIViewController {
 
 IO.puts("Elixir: functional, concurrent, bug cũng concurrent")
 
+#include <iostream>
+
+int main() {
+    std::cout << "C++: mạnh mẽ, phức tạp, đau đầu" << std::endl;
+    return 0;
+}
+
