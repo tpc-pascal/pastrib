f95d6a824272504f931413579661e23da861ad13
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-02-10 18:04:23 +0700

    daily: 2025-02-10

--- dummy/2026-06-17/2025-02-10.cpp
+++ dummy/2026-06-17/2025-02-10.cpp
@@ -2,3 +2,10 @@ fn main() {
     println!("Nó compile được? Bất ngờ chưa!");
 }
 
+#include <iostream>
+
+int main() {
+    std::cout << "C++: mạnh mẽ, phức tạp, đau đầu" << std::endl;
+    return 0;
+}
+
