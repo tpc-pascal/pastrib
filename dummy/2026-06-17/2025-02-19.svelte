36b5ade941b2587605d40126a07652895c788a73
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-02-19 18:23:34 +0700

    daily: 2025-02-19

--- dummy/2026-06-17/2025-02-19.svelte
+++ dummy/2026-06-17/2025-02-19.svelte
@@ -1,2 +1,9 @@
 const [isWorking, setIsWorking] = useState(false)  // luôn false
 
+function debug() {
+    console.log("1")
+    console.log("2")
+    console.log("debug hết")
+    // chắc bug ở đâu đó gần đây
+}
+
