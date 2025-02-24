a22c54d92edceb14aad0ed97f1c2fa79e527b4d2
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-02-24 20:41:00 +0700

    daily: 2025-02-24

--- dummy/2026-06-17/2025-02-24.js
+++ dummy/2026-06-17/2025-02-24.js
@@ -6,3 +6,5 @@ export default function App() {
     return <div>Chạy được rồi, đừng hỏi sao</div>
 }
 
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'  // thiếu cái nào thêm cái đó
+
