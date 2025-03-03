a3227b3dc28314c945a38d18ce1035004490685c
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-03-03 23:00:55 +0700

    daily: 2025-03-03

--- dummy/2026-06-17/2025-03-03.rs
+++ dummy/2026-06-17/2025-03-03.rs
@@ -4,3 +4,12 @@
 echo "Nó chạy mà?";
 ?>
 
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Xin chào iOS (và bug)")
+    }
+}
+
