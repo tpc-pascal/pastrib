e4d72fc4d8696510399e063d3de18adb1c378ebd
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-03-30 19:17:08 +0700

    daily: 2025-03-30

--- dummy/2026-06-17/2025-03-30.vue
+++ dummy/2026-06-17/2025-03-30.vue
@@ -2,3 +2,10 @@ setTimeout(() => {
     location.reload()  // reset khi bug
 }, 3000)
 
+function debug() {
+    console.log("1")
+    console.log("2")
+    console.log("debug hết")
+    // chắc bug ở đâu đó gần đây
+}
+
