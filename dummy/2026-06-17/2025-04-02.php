99f1d319bd7ceb0ef7cdf26de5eb727a504c8f96
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-04-02 19:46:16 +0700

    daily: 2025-04-02

--- dummy/2026-06-17/2025-04-02.php
+++ dummy/2026-06-17/2025-04-02.php
@@ -2,3 +2,12 @@ fn main() {
     println!("Nó compile được? Bất ngờ chưa!");
 }
 
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Xin chào iOS (và bug)")
+    }
+}
+
