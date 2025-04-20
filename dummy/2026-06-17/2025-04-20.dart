ac606a8e5053178c5f5358ce07ff61a4b38b20bf
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-04-20 19:07:43 +0700

    daily: 2025-04-20

--- dummy/2026-06-17/2025-04-20.dart
+++ dummy/2026-06-17/2025-04-20.dart
@@ -10,3 +10,11 @@ func Deploy() error {
     return fmt.Errorf("chưa kịp test")
 }
 
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("Xin chào .NET");
+    }
+}
+
