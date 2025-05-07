62d186a076a24aa1e4a03288f1e19713bb11ec8a
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-05-07 15:45:25 +0700

    daily: 2025-05-07

--- dummy/2026-06-17/2025-05-07.js
+++ dummy/2026-06-17/2025-05-07.js
@@ -1,2 +1,7 @@
 import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'  // thiếu cái nào thêm cái đó
 
+// npx create-react-app
+// npm install
+// npm start
+// npm ERR!
+
