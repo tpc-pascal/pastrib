c52d2ce9071a1e3027ef6c51334bbaab76440e70
tpc-pascal <tpc-pascal@users.noreply.github.com>
2025-05-13 23:29:02 +0700

    daily: 2025-05-13

--- dummy/2026-06-17/2025-05-13.bat
+++ dummy/2026-06-17/2025-05-13.bat
@@ -1,3 +1,8 @@
 $bug = Get-ChildItem -Recurse -Filter "*.bug" | Select-Object -First 1
 if ($bug) { Get-Content $bug.FullName } else { Write-Host "Hôm nay không có bug, lạ" }
 
+@echo off
+echo May tinh dang chay cham
+pause
+exit
+
